97fb799a054c45954d00460af9bfd79ae8503ee2
Emilijus Stankus <emilijus@gmail.com>
2017-05-23 19:04:20 +0300

    Finished Edge class

---
+++ ODMRPprototype/Edge.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODMRPprototype
+{
+    class Edge
+    {
+        int Weight;
+        Queue<Pair<Packet, int>> Packets;
+        Node Destination;
+
+        public Edge(int weight, Node destination)
+        {
+            this.Weight = weight;
+            this.Destination = destination;
+        }
+
+        public void Send(Packet packet)
+        {
+            Packets.Enqueue(new Pair<Packet, int>(packet, Weight));
+        }
+
+        public void Update()
+        {
+            foreach(var p in Packets.ToList())
+            {
+                if(--p.Second == 0)
+                {
+                    Destination.IncomingData(Packets.Dequeue().First);
+                }
+            }
+        }
+    }
+}
---
+++ ODMRPprototype/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODMRPprototype
+{
+    class Node
+    {
+        Queue<Packet> Packets;
+
+        public void IncomingData(Packet packet)
+        {
+            Packets.Enqueue(packet);
+        }
+    }
+}
